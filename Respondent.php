60449528695719a0efd088e5928e77a6337ef970
Tõnis Ormisson <tonis@andmemasin.eu>
2017-02-03 15:14:14 +0200

    fix ui

--- Respondent.php
+++ Respondent.php
@@ -4,12 +4,14 @@ namespace andmemasin\surveybasemodels;
 
 use andmemasin\myabstract\MyActiveRecord;
 use andmemasin\surveybasemodels\Survey;
+use yii;
 
 /**
  * This is the model class for a generic Respondent.
  *
  * @property int $respondent_id
  * @property int $survey_id
+ * @property string $token
  * @property Survey $survey
  *
  */
@@ -23,7 +25,17 @@ class Respondent extends MyActiveRecord
         return array_merge([
             [['survey_id'], 'required'],
             [['survey_id'], 'integer'],
+            [['token'], 'unique'],
         ], parent::rules());
+    }
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'token' => Yii::t('app', 'Unique Token'),
+        ];
     }
 
 
