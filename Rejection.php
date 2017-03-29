3c93bd7cf12d3ab7e9682e31b504b7a007ca5ff1
Tõnis Ormisson <tonis@andmemasin.eu>
2017-03-29 10:06:15 +0300

    add rejection

---
+++ Rejection.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace andmemasin\surveybasemodels;
+use andmemasin\myabstract\MyActiveRecord;
+use yii;
+
+/**
+ *
+ * @property integer $rejection_id
+ * @property integer $survey_id
+ * @property integer $respondent_id
+ * @property string $email_address
+ * @property string $type
+ * @property string $bounce
+ * @property string $time_rejected
+ *
+ * @property Respondent $respondent
+ * @property Survey $survey
+ */
+class Rejection extends MyActiveRecord
+{
+
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'rejection';
+    }
+    
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return array_merge([
+            [['survey_id', 'respondent_id'], 'integer'],
+            [['email_address','time_rejected'], 'required'],
+            [['bounce'], 'string'],
+            [['time_rejected'], 'safe'],
+            [['email_address'], 'string', 'max' => 255],
+            [['email_address'], 'email'],
+            [['type'], 'string', 'max' => 45],
+            [['respondent_id'], 'exist', 'skipOnError' => true, 'targetClass' => Respondent::className(), 'targetAttribute' => ['respondent_id' => 'id']],
+            [['survey_id'], 'exist', 'skipOnError' => true, 'targetClass' => Survey::className(), 'targetAttribute' => ['survey_id' => 'id']],
+        ],  parent::rules());
+    }
+
+
+    /**
+     * @param string $email_address
+     * @return bool
+     */
+    public static function hasBouncedHard($email_address){
+        $rejection = Rejection::findByEmail($email_address);
+        if($rejection){
+            return true;
+        }
+        return false;
+    }
+
+
+
+    /**
+     * @param string $email_address
+     * @return Rejection[]
+     */
+    public static function findHardBounces($email_address){
+        return Rejection::find()
+            ->andWhere(['email_address'=>$email_address])
+
+            ->all();
+    }
+
+    /**
+     * Check whether there are rejections that match the respondent's token
+     * @param string $code
+     * @return bool
+     */
+    public static function rejectedByCode($code){
+        $response = Response::findByKey($code);
+        if($response){
+            $rejections = self::find()
+                ->andWhere(['respondent_id'=>$response->respondent->primaryKey])
+                ->all();
+            if($rejections){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Check whether we have a bounce registered from this email
+     * @param string $email_address
+     * @param string $type
+     * @return bool
+     */
+    public static function bouncedByEmailAddress($email_address,$type = self::BOUNCE_TYPE_HARD){
+        $rejections = self::find()
+            ->andWhere('email_address=:email_address', [':email_address' => $email_address])
+            ->andWhere('type=:type', [':type' => $type])
+            ->all();
+        if($rejections){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @param string $email_address
+     * @return Rejection
+     */
+    public static function findByEmail($email_address){
+        return Rejection::findOne(['email_address'=>$email_address]);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getSurvey()
+    {
+        return $this->hasOne(Survey::className(), ['id' => 'survey_id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getRespondent()
+    {
+        return $this->hasOne(Respondent::className(), ['id' => 'respondent_id']);
+    }
+}
