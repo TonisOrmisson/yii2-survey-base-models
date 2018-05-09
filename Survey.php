4ac522f7bf60aae5a8cbdd8eaa6af9de36578c12
Tõnis Ormisson <tonis@andmemasin.eu>
2018-05-09 11:38:42 +0300

    DEV: remove Collector dependency from basemodels

--- Survey.php
+++ Survey.php
@@ -3,9 +3,7 @@
 namespace andmemasin\surveybasemodels;
 
 use andmemasin\myabstract\ModelWithHasStatus;
-use andmemasin\collector\models\Collector;
 use yii;
-use andmemasin\collector\models\CollectorInterface;
 
 /**
  * This is the model class for a generic Survey. This describes common
@@ -17,11 +15,8 @@ use andmemasin\collector\models\CollectorInterface;
  * @property string $key
  * @property string $status
  * @property string $name
- * @property integer $collector_id
- * @property string $options The options as json string. Contains the
- * Collector authentication information (or any other info)
+ * @property string $options The options as json string. Contains the Collector authentication information (or any other info)
  *
- * @property Collector $collector
  *
  */
 class Survey extends ModelWithHasStatus
@@ -38,7 +33,6 @@ class Survey extends ModelWithHasStatus
             [['name','status'], 'required'],
             [['name', 'status'], 'string', 'max' => 255],
             [['name'], 'string','max' => 500],
-            [['collector_id'], 'integer'],
             [['options'], 'string','max' => 1024 * 10],
         ], parent::rules());
     }
@@ -51,7 +45,6 @@ class Survey extends ModelWithHasStatus
             'survey_id' => Yii::t('app', 'ID'),
             'name' => Yii::t('app', 'Survey name'),
             'status' => Yii::t('app', 'Status'),
-            'collector_id' => Yii::t('app', 'Data collection interface'),
             'options' => Yii::t('app', 'Options JSON'),
             'key' => Yii::t('app', 'Survey key'),
         ];
@@ -59,7 +52,6 @@ class Survey extends ModelWithHasStatus
     public function attributeHints()
     {
         return [
-            'collector_id' => Yii::t('app', 'The data collection interface allows API access to collected data to identify status or respondents in real time. E.g. whether respondent has already answered or not.'),
             'key' => Yii::t('app', 'Survey key is a cross-platform multi-mode unique id for survey.'),
         ];
     }
@@ -69,25 +61,5 @@ class Survey extends ModelWithHasStatus
     }
 
 
-    /**
-     * @return CollectorInterface
-     * @throws yii\base\InvalidConfigException
-     */
-    public function getCollector()
-    {
-        $model = Collector::findOne($this->collector_id);
-        if($model){
-
-            /** @var Collector $collector */
-            $collector = Yii::createObject([
-                'class' => $model->getClassName(),
-                'survey'=>$this,
-                'collector_id' => $this->collector_id,
-                'type'=>$model->type,
-            ]);
-            return $collector;
-        }
-        return null;
-    }
 
 }
