a2a5a4a6fd3553963d273c95f13a7ea6c2ba0f1f
Tõnis Ormisson <tonis@andmemasin.eu>
2018-07-24 11:15:56 +0300

    test: Test e-mail format

--- tests/unit/RespondentTest.php
+++ tests/unit/RespondentTest.php
@@ -7,6 +7,8 @@ require_once __DIR__ . '/../TestBaseActive.php';
 
 use andmemasin\surveybasemodels\Respondent;
 use andmemasin\surveybasemodels\tests\TestBaseActive;
+use Codeception\Stub;
+use yii\base\Model;
 
 /**
  * @author Tõnis Ormisson <tonis@andmemasin.eu>
@@ -35,6 +37,75 @@ class RespondentTest extends TestBaseActive
             'time_collector_registered' => null,
 
         ], parent::baseModelAttributes());
+    }
+
+
+    /**
+     * @return array
+     */
+    public function badAddressesProvider(){
+        return [
+            ['my email address'],
+            ['my .name@gmail.com'],
+            ['my.name @gmail.com'],
+            ['my.name@gmail. com'],
+            ['my.name@gmail,com'],
+            ['my.name@gmail;com'],
+            ['my.name@gmail,com'],
+            [null],
+            [0],
+            [1.234],
+        ];
+    }
+
+
+    /**
+     * @return array
+     */
+    public function goodAddressesProvider(){
+        return [
+            ['name@gmail.com'],
+            ['my.name@gmail.com'],
+            ['mY.nAmE@gmAil.CoM'],
+            ['name@amazon.Co.uk'],
+        ];
+    }
+
+
+    /**
+     * @dataProvider badAddressesProvider
+     */
+    public function testValidateEmailFails($address) {
+        $this->model->email_address = $address;
+        $this->assertFalse($this->model->validateEmail('email_address', $this->model->email_address));
+    }
+
+    /**
+     * @dataProvider goodAddressesProvider
+     */
+    public function testValidateEmailPasses($address) {
+        $this->model->email_address = $address;
+        /** @var Model $model */
+        $this->model = Stub::make($this->modelClass, [
+            'attributes' => array_keys($this->baseModelAttributes()),
+            'isEmailSurveyDuplicate' => false,
+        ]);
+        $this->model->setAttributes($this->baseModelAttributes());
+        $this->assertTrue($this->model->validateEmail('email_address', $this->model->email_address));
+    }
+
+    /**
+     * @dataProvider goodAddressesProvider
+     */
+    public function testValidateEmailFailsDuplicate($address) {
+        $this->model->email_address = $address;
+        /** @var Model $model */
+        $this->model = Stub::make($this->modelClass, [
+            'attributes' => array_keys($this->baseModelAttributes()),
+            'isEmailSurveyDuplicate' => true,
+        ]);
+        $this->model->setAttributes($this->baseModelAttributes());
+        $this->assertFalse($this->model->validateEmail('email_address', $this->model->email_address));
     }
 
 
